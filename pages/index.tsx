b1006d58e6d8e2343a0ab29cd59714f53dc30379
Nitin Kanish <nitinkanish@gmail.com>
2023-01-18 13:44:59 +0530

    Updated the className

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,14 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <section>
-  <div class="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+  <div className="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
     <div
-      class="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16"
+      className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16"
     >
-      <div class="mx-auto max-w-lg text-center lg:mx-0 lg:text-left">
-        <h2 class="text-3xl font-bold sm:text-4xl">Find your career path</h2>
+      <div className="mx-auto max-w-lg text-center lg:mx-0 lg:text-left">
+        <h2 className="text-3xl font-bold sm:text-4xl">Find your career path</h2>
 
-        <p class="mt-4 text-gray-600">
+        <p className="mt-4 text-gray-600">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero
           aliquid sint distinctio iure ipsum cupiditate? Quis, odit assumenda?
           Deleniti quasi inventore, libero reiciendis minima aliquid tempora.
@@ -27,13 +27,13 @@ const Home: NextPage = () => {
         </p>
 
         <a
-          class="mt-8 inline-flex items-center rounded border border-indigo-600 bg-indigo-600 px-8 py-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+          className="mt-8 inline-flex items-center rounded border border-indigo-600 bg-indigo-600 px-8 py-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
           href="/get-started"
         >
-          <span class="text-sm font-medium"> Get Started </span>
+          <span className="text-sm font-medium"> Get Started </span>
 
           <svg
-            class="ml-3 h-5 w-5"
+            className="ml-3 h-5 w-5"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -49,14 +49,14 @@ const Home: NextPage = () => {
         </a>
       </div>
 
-      <div class="grid grid-cols-2 gap-4 sm:grid-cols-3">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
         <a
-          class="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
           href="/accountant"
         >
-          <span class="inline-block rounded-lg bg-gray-50 p-3">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
             <svg
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -75,20 +75,20 @@ const Home: NextPage = () => {
             </svg>
           </span>
 
-          <h2 class="mt-2 font-bold">Accountant</h2>
+          <h2 className="mt-2 font-bold">Accountant</h2>
 
-          <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
             Lorem ipsum dolor sit amet consectetur.
           </p>
         </a>
 
         <a
-          class="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
           href="/accountant"
         >
-          <span class="inline-block rounded-lg bg-gray-50 p-3">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
             <svg
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -107,20 +107,20 @@ const Home: NextPage = () => {
             </svg>
           </span>
 
-          <h2 class="mt-2 font-bold">Accountant</h2>
+          <h2 className="mt-2 font-bold">Accountant</h2>
 
-          <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
             Lorem ipsum dolor sit amet consectetur.
           </p>
         </a>
 
         <a
-          class="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
           href="/accountant"
         >
-          <span class="inline-block rounded-lg bg-gray-50 p-3">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
             <svg
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -139,20 +139,20 @@ const Home: NextPage = () => {
             </svg>
           </span>
 
-          <h2 class="mt-2 font-bold">Accountant</h2>
+          <h2 className="mt-2 font-bold">Accountant</h2>
 
-          <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
             Lorem ipsum dolor sit amet consectetur.
           </p>
         </a>
 
         <a
-          class="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
           href="/accountant"
         >
-          <span class="inline-block rounded-lg bg-gray-50 p-3">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
             <svg
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -171,20 +171,20 @@ const Home: NextPage = () => {
             </svg>
           </span>
 
-          <h2 class="mt-2 font-bold">Accountant</h2>
+          <h2 className="mt-2 font-bold">Accountant</h2>
 
-          <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
             Lorem ipsum dolor sit amet consectetur.
           </p>
         </a>
 
         <a
-          class="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
           href="/accountant"
         >
-          <span class="inline-block rounded-lg bg-gray-50 p-3">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
             <svg
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -203,20 +203,20 @@ const Home: NextPage = () => {
             </svg>
           </span>
 
-          <h2 class="mt-2 font-bold">Accountant</h2>
+          <h2 className="mt-2 font-bold">Accountant</h2>
 
-          <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
             Lorem ipsum dolor sit amet consectetur.
           </p>
         </a>
 
         <a
-          class="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
           href="/accountant"
         >
-          <span class="inline-block rounded-lg bg-gray-50 p-3">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
             <svg
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -235,9 +235,9 @@ const Home: NextPage = () => {
             </svg>
           </span>
 
-          <h2 class="mt-2 font-bold">Accountant</h2>
+          <h2 className="mt-2 font-bold">Accountant</h2>
 
-          <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
             Lorem ipsum dolor sit amet consectetur.
           </p>
         </a>
